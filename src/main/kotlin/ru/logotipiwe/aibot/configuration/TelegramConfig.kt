437de4e993881b229836aefa48a71ced300a415e
logotipiwe <logotipiwe@example.com>
2024-09-26 23:23:30 +0300

    Init

--- src/main/kotlin/ru/logotipiwe/aibot/configuration/TelegramConfig.kt
+++ src/main/kotlin/ru/logotipiwe/aibot/configuration/TelegramConfig.kt
@@ -2,18 +2,11 @@ package ru.logotipiwe.aibot.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
-import org.telegram.telegrambots.meta.generics.TelegramClient
 import ru.logotipiwe.aibot.model.Bot
 
 @Configuration
 class TelegramConfig {
-    @Bean
-    fun tgClient(): TelegramClient {
-        return OkHttpTelegramClient("7811476690:AAGf7JD72Hc2vRQXqGkL3NxdDWskqhtTFzU")
-    }
-
     @Bean
     fun aiRoflBot(
         bots: List<Bot>
